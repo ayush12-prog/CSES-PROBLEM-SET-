d98e6d087c129d512b780250774481ce570d2177
Ayush Kumar <ayushkumar991890@gmail.com>
2025-12-27 18:06:21 +0530

    Add CSES problem 1071: Number Spiral solution

---
+++ CSES_Problems/1071-Number_Spiral/solution.cpp
@@ -0,0 +1,37 @@
+// CSES Problem 1071: Number Spiral
+// Link: https://cses.fi/problemset/task/1071/
+// Description: A number spiral is an infinite grid whose upper-left square has number 1. Here a...
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            long long y, x;
+            cin >> y >> x;
+            long long z = max(y, x);
+            long long ans;
+            if (z % 2 == 0) {
+                if (y == z) {
+                    ans = z * z - x + 1;
+                } else {
+                    ans = (z - 1) * (z - 1) + y;
+                }
+            } else {
+                if (y == z) {
+                    ans = (z - 1) * (z - 1) + x;
+                } else {
+                    ans = z * z - y + 1;
+                }
+            }
+            cout << ans << "\n";
+        }
+    }
+    return 0;
+}
