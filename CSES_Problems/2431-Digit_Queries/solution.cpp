2f0e1ddf9a5d153ccf87cf77aa7741d507b85f4e
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-12 21:26:23 +0530

    Add CSES problem 2431: Digit Queries solution

---
+++ CSES_Problems/2431-Digit_Queries/solution.cpp
@@ -0,0 +1,42 @@
+// CSES Problem 2431: Digit Queries
+// Link: https://cses.fi/problemset/task/2431/
+// Description: Consider an infinite string that consists of all positive integers in increasing...
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    long long k;
+    cin >> k;
+
+    long long len = 1;
+    long long count = 9;
+    long long start = 1;
+
+    while (k > len * count) {
+        k -= len * count;
+        len++;
+        count *= 10;
+        start *= 10;
+    }
+
+    start += (k - 1) / len;
+    string s = to_string(start);
+    cout << s[(k - 1) % len] << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int q;
+    if (cin >> q) {
+        while (q--) {
+            solve();
+        }
+    }
+    return 0;
+}
