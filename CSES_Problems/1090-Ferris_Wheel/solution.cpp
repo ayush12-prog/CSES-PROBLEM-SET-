87faf50b9577f3aa8bae83c4c0d838dd22249ae2
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-16 18:21:20 +0530

    Add CSES problem 1090: Ferris Wheel solution

---
+++ CSES_Problems/1090-Ferris_Wheel/solution.cpp
@@ -0,0 +1,42 @@
+// CSES Problem 1090: Ferris Wheel
+// Link: https://cses.fi/problemset/task/1090/
+// Description: There are n children who want to go to a Ferris wheel, and your task is to find ...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    long long x;
+    if (cin >> n >> x) {
+        vector<long long> p(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> p[i];
+        }
+
+        sort(p.begin(), p.end());
+
+        int i = 0;
+        int j = n - 1;
+        int gondolas = 0;
+
+        while (i <= j) {
+            if (p[i] + p[j] <= x) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            gondolas++;
+        }
+
+        cout << gondolas << endl;
+    }
+    return 0;
+}
