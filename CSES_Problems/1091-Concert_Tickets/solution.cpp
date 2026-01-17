34651d66aa138ca4cd0ed434a057311bad200de3
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-17 22:51:59 +0530

    Add CSES problem 1091: Concert Tickets solution

---
+++ CSES_Problems/1091-Concert_Tickets/solution.cpp
@@ -0,0 +1,48 @@
+// CSES Problem 1091: Concert Tickets
+// Link: https://cses.fi/problemset/task/1091/
+// Description: There are n concert tickets available, each with a certain price. Then, m custom...
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    if (!(cin >> n >> m)) return;
+
+    multiset<int> tickets;
+    for (int i = 0; i < n; ++i) {
+        int h;
+        cin >> h;
+        tickets.insert(h);
+    }
+
+    for (int i = 0; i < m; ++i) {
+        int t;
+        cin >> t;
+
+        // Find the first element strictly greater than t
+        auto it = tickets.upper_bound(t);
+
+        // If the iterator is at the beginning, all elements are > t
+        if (it == tickets.begin()) {
+            cout << -1 << "\n";
+        } else {
+            // Decrement to find the largest element <= t
+            --it;
+            cout << *it << "\n";
+            // Remove the ticket from the pool
+            tickets.erase(it);
+        }
+    }
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
