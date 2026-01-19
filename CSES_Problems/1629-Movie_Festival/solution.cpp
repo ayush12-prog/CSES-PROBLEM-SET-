37c26b24ed8689224d0e60b3ee6fc462c8462ff5
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-19 18:11:50 +0530

    Add CSES problem 1629: Movie Festival solution

---
+++ CSES_Problems/1629-Movie_Festival/solution.cpp
@@ -0,0 +1,45 @@
+// CSES Problem 1629: Movie Festival
+// Link: https://cses.fi/problemset/task/1629/
+// Description: In a movie festival n movies will be shown. You know the starting and ending tim...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Movie {
+    int start, end;
+};
+
+bool compareMovies(const Movie& a, const Movie& b) {
+    return a.end < b.end;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (cin >> n) {
+        vector<Movie> movies(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> movies[i].start >> movies[i].end;
+        }
+
+        sort(movies.begin(), movies.end(), compareMovies);
+
+        int count = 0;
+        int current_end_time = 0;
+
+        for (const auto& m : movies) {
+            if (m.start >= current_end_time) {
+                count++;
+                current_end_time = m.end;
+            }
+        }
+
+        cout << count << endl;
+    }
+    return 0;
+}
