513af819c153f3db126c2e1113f5837a94a89e27
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-22 18:59:12 +0530

    Add CSES problem 2183: Missing Coin Sum solution

---
+++ CSES_Problems/2183-Missing_Coin_Sum/solution.cpp
@@ -0,0 +1,36 @@
+// CSES Problem 2183: Missing Coin Sum
+// Link: https://cses.fi/problemset/task/2183/
+// Description: You have n coins with positive integer values. What is the smallest sum you cann...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<long long> x(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+
+    sort(x.begin(), x.end());
+
+    long long target = 1;
+    for (int i = 0; i < n; i++) {
+        if (x[i] > target) {
+            break;
+        }
+        target += x[i];
+    }
+
+    cout << target << endl;
+
+    return 0;
+}
