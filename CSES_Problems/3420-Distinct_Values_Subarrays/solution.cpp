1565e80fd50df67f2f99815bcc8c46df3f7cf4ec
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-28 21:21:58 +0530

    Add CSES problem 3420: Distinct Values Subarrays solution

---
+++ CSES_Problems/3420-Distinct_Values_Subarrays/solution.cpp
@@ -0,0 +1,39 @@
+// CSES Problem 3420: Distinct Values Subarrays
+// Link: https://cses.fi/problemset/task/3420/
+// Description: Given an array of n integers, count the number of subarrays where each element i...
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (cin >> n) {
+        vector<int> x(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> x[i];
+        }
+
+        map<int, int> last_pos;
+        long long count = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; ++right) {
+            if (last_pos.count(x[right])) {
+                left = max(left, last_pos[x[right]] + 1);
+            }
+
+            last_pos[x[right]] = right;
+            count += (right - left + 1);
+        }
+
+        cout << count << endl;
+    }
+    return 0;
+}
