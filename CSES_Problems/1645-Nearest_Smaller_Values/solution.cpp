d31f86b80ccc08379cbaf2691a653514f6cc281a
Ayush Kumar <ayushkumar991890@gmail.com>
2026-03-21 20:37:39 +0530

    Add CSES problem 1645: Nearest Smaller Values solution

---
+++ CSES_Problems/1645-Nearest_Smaller_Values/solution.cpp
@@ -0,0 +1,36 @@
+// CSES Problem 1645: Nearest Smaller Values
+// Link: https://cses.fi/problemset/task/1645/
+// Description: Given an array of n integers, your task is to find for each array position the n...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<long long> x(n);
+    for (int i = 0; i < n; i++) cin >> x[i];
+
+    stack<int> st; // stores indices
+
+    for (int i = 0; i < n; i++) {
+        // Remove elements >= current
+        while (!st.empty() && x[st.top()] >= x[i]) {
+            st.pop();
+        }
+
+        // Output result
+        if (st.empty()) cout << 0 << " ";
+        else cout << st.top() + 1 << " "; // +1 for 1-based index
+
+        // Push current index
+        st.push(i);
+    }
+
+    cout << "\n";
+    return 0;
+}
